33e7bdd41abe02a0fc30d7f0b6bea1dbf26f0dc3
Francis Ong <francis.ong25@gmail.com>
2023-10-22 18:23:11 +0800

    feat: add sensor delete controller

--- controllers/sensor.controller.js
+++ controllers/sensor.controller.js
@@ -1,9 +1,10 @@
 const { Sensor } = require('../models');
+const mongoose = require('mongoose');
 const generateSensorData = require('../utils/generateSensorData');
 
 /**
  * Create a sensor data
- * @route POST /create
+ * @route POST /sensor/create
  */
 exports.createSensor = async (_req, res, next) => {
   try {
@@ -11,7 +12,32 @@ exports.createSensor = async (_req, res, next) => {
 
     await newSensorData.save();
 
-    res.send({ message: 'Sensor created' });
+    res.status(201).json({ message: 'Sensor created' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Delete a sensor data
+ * @route DELETE /sensor/:id
+ */
+exports.deleteSensor = async (req, res, next) => {
+  try {
+    const { id: sensorId } = req.params;
+    const isValidId = mongoose.isValidObjectId(sensorId);
+
+    if (!isValidId) {
+      return res.status(400).json({ message: 'Invalid ObjectID' });
+    }
+
+    const existingSensor = await Sensor.findByIdAndDelete(sensorId);
+
+    if (!existingSensor) {
+      return res.status(404).json({ message: 'Sensor not found' });
+    }
+
+    res.status(204).send();
   } catch (error) {
     next(error);
   }
