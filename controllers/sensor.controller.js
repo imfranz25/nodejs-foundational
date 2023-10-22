73911b9287842e2c3d672b94acd6db2544fb117b
Francis Ong <francis.ong25@gmail.com>
2023-10-22 19:28:29 +0800

    feat: wrap up - CRUD for sensor

--- controllers/sensor.controller.js
+++ controllers/sensor.controller.js
@@ -3,6 +3,46 @@ const mongoose = require('mongoose');
 const generateSensorData = require('../utils/generateSensorData');
 
 /**
+ * Get sensor data
+ * @route GET /sensor/:id
+ */
+exports.getSensor = async (req, res, next) => {
+  try {
+    const { id: sensorId } = req.params;
+
+    const isValidId = mongoose.isValidObjectId(sensorId);
+
+    if (!isValidId) {
+      return res.status(400).json({ message: 'Invalid ObjectID' });
+    }
+
+    const sensorData = await Sensor.findById(sensorId);
+
+    if (!sensorData) {
+      return res.status(404).json({ message: 'Sensor not found' });
+    }
+
+    res.status(200).json({ message: 'Sensor fetched', sensor: sensorData });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Get all sensor data
+ * @route GET /sensor
+ */
+exports.getAllSensor = async (_req, res, next) => {
+  try {
+    const sensorData = await Sensor.find({});
+
+    res.status(200).json({ message: 'Sensors fetched', sensor: sensorData });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
  * Create a sensor data
  * @route POST /sensor/create
  */
--- routes/sensor.route.js
+++ routes/sensor.route.js
@@ -1,12 +1,39 @@
 const express = require('express');
+
+const { sensorValidator } = require('../validators');
+const { sensorController } = require('../controllers');
+const { validatorMiddleware } = require('../middlewares');
+
+/* Initialization */
 const router = express.Router();
 
-/* File Imports */
-const { sensorController } = require('../controllers');
+/**
+ * --------------
+ * @route /sensor
+ * --------------
+ */
 
-// prettier-ignore
+/* Get all sensor */
+router.route('/').get(sensorController.getAllSensor);
+
+/* Create */
 router
   .route('/create')
+  .all(sensorValidator.createSensorValidator)
+  .all(validatorMiddleware)
   .post(sensorController.createSensor);
+
+/* Get and Delete */
+router
+  .route('/:id')
+  .get(sensorController.getSensor)
+  .delete(sensorController.deleteSensor);
+
+/* Update */
+router
+  .route('/:id')
+  .all(sensorValidator.createSensorValidator)
+  .all(validatorMiddleware)
+  .put(sensorController.updateSensor);
 
 module.exports = router;
