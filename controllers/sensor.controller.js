c0d7585e7c982f3793f7cf302f7cff07f5a90180
Francis Ong <francis.ong25@gmail.com>
2023-10-22 19:01:15 +0800

    feat: add controller for updating sensor data

--- controllers/sensor.controller.js
+++ controllers/sensor.controller.js
@@ -12,7 +12,7 @@ exports.createSensor = async (_req, res, next) => {
 
     await newSensorData.save();
 
-    res.status(201).json({ message: 'Sensor created' });
+    res.status(201).json({ message: 'Sensor created', sensor: newSensorData });
   } catch (error) {
     next(error);
   }
@@ -38,6 +38,36 @@ exports.deleteSensor = async (req, res, next) => {
     }
 
     res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Update sensor data
+ * @route PUT /sensor/:id
+ */
+exports.updateSensor = async (req, res, next) => {
+  try {
+    const { id: sensorId } = req.params;
+
+    const isValidId = mongoose.isValidObjectId(sensorId);
+
+    if (!isValidId) {
+      return res.status(400).json({ message: 'Invalid ObjectID' });
+    }
+
+    const updatedSensor = await Sensor.findByIdAndUpdate(
+      sensorId,
+      { ...req.body },
+      { new: true }
+    );
+
+    if (!updatedSensor) {
+      return res.status(404).json({ message: 'Sensor not found' });
+    }
+
+    res.status(200).json({ message: 'Sensor updated', sensor: updatedSensor });
   } catch (error) {
     next(error);
   }
